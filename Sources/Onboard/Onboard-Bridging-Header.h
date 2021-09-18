6a648f16b91e7fb920ee021b9bf808a2927e36a4
Malcolmn Roberts <36816789+MalcolmnDEV@users.noreply.github.com>
2021-09-18 11:04:33 +0100

    bridging header

---
+++ Sources/Onboard/Onboard-Bridging-Header.h
@@ -0,0 +1,3 @@
+#import "OnboardingContentViewController.h"
+#import "OnboardingViewController.h"
+
---
+++ Sources/Onboard/Onboard.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Malcolmn Roberts on 18/09/2021.
+//
+
+import Foundation
